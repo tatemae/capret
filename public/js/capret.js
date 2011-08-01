eb4cfd2ee86567caf62ed4c52b13f4fce4544ac1
Justin Ball <justinball@gmail.com>
2011-08-01 14:12:50 -0600

    Added jQuery scope

--- public/js/capret.js
+++ public/js/capret.js
@@ -1,16 +1,19 @@
-jQuery(function() {
-  var tracking_url = 'http://stats.oerglue.com:8000/tracking.gif';
-  var env = {};
-  env.u = document.location.href;
-  env.bw = window.innerWidth;
-  env.bh = window.innerHeight;
-  if (document.referrer && document.referrer != "") {
-    env.ref = document.referrer;
-  }
-  var track = '<img src="' + tracking_url + '&' + jQuery.param(env) + '"/>';
-	var license = oer_license_parser.get_license();
-	jQuery('body').clipboard({
-    append: license.license_html + track,
-    oncopy: function(e) { console.log(e); }
-  });	
+(function( $ ){
+	jQuery = $ = $.noConflict(true);
+	jQuery(function() {
+	  var tracking_url = 'http://stats.oerglue.com:8000/tracking.gif';
+	  var env = {};
+	  env.u = document.location.href;
+	  env.bw = window.innerWidth;
+	  env.bh = window.innerHeight;
+	  if (document.referrer && document.referrer != "") {
+	    env.ref = document.referrer;
+	  }
+	  var track = '<img src="' + tracking_url + '&' + jQuery.param(env) + '"/>';
+		var license = oer_license_parser.get_license();
+		jQuery('body').clipboard({
+	    append: license.license_html + track,
+	    oncopy: function(e) { console.log(e); }
+	  });	
+	});
 });
