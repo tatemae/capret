564ffc527cde16dfe07e2154ab6a49816daba4a9
Justin Ball <justinball@gmail.com>
2011-11-10 18:03:55 -0700

    Added visits fields to tabular data

--- public/js/tabular.js
+++ public/js/tabular.js
@@ -13,10 +13,14 @@ Hummingbird.Tabular.init = function() {
 	  var rowTemplate = $(".row_template");
 	  $.each(data, function(index, item){
 	  	var row = rowTemplate.clone();
-	    row.find('td.ip').text(item.ip);
-			row.find('td.date').text(item.minute);
+	    row.find('td.date').text(item.timestamp);
+			row.find('td.ip').text(item.ip);
+			row.find('td.ua').text(item.ua);			
+			row.find('td.referrer').text(item.ref);
+			row.find('td.location').text(item.u);
+			row.find('td.time').text(item.time);			
+			row.find('td.stuff').text(JSON.stringify(item));			
 	    row.appendTo('#tabular-data');
 	  });
 	});
 };
-
